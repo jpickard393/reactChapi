93d225c5124daa4ddb18472ee6381bf505e00ab8
jpickard393 <jpickard@and.digital>
2022-07-15 15:33:52 +0100

    removed test file

--- src/globalConstants.js
+++ src/globalConstants.js
@@ -1,3 +1,2 @@
 export const apiKey = "ODExYmZmODQtZDM0Mi00OWM0LWE2NWQtZWFiNTFlZmMwNThjOg==";
 export const baseUrl = "https://localhost:7269/api/";
-//export const baseUrl = "https://api.company-information.service.gov.uk/";
